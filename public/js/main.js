af3880a808110e82340287151d6976904ea150d2
Handle Huang <handle.huang0409@gmail.com>
2015-04-23 17:59:07 +0800

    ab

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,87 @@
-$(function(){
-  var imageList = [],preloadImageList=[];
-  function init(){
+$(function() {
+  var imageList = [],
+    preloadImageList = [],
+    totalPages = 0,
+    pageIndex = 0,
+    thumbWidth = 200,
+    gutter = 20,
+    curLeft = 0,
+    curTop = 100,
+    chatperWidth = 50;
+
+  function init() {
     // init books
-    _.each(BOOK_DETAILS,function(obj,idx,arr){
+    _.each(BOOK_DETAILS, function(obj, idx, arr) {
       imageList.push(obj.chapter);
       imageList = imageList.concat(obj.thumbs);
       imageList = imageList.concat(obj.pages);
+      totalPages += obj.pages.length;
     });
-    _.each(imageList,function(val,index,arr){
+    _.each(imageList, function(val, index, arr) {
       preloadImageList.push({
-        id:"book_"+index,
-        src:BASE_PATH+"/"+val
+        id: "book_" + index,
+        src: BASE_PATH + "/" + val
       });
     });
     preloadimg({
-      list:preloadImageList,
-      progress:_progress,
-      end:_end
+      list: preloadImageList,
+      progress: _progress,
+      end: _end
     });
   }
-  function _progress(data){}
-  function _end(data){}
 
+  function _progress(data) {
+    //console.log(data.img.src);
+    addChapter(data);
+    addThumb(data);
+    addPage(data);
+  }
+
+  function _getImageName(path) {
+    var ps = path.split('/');
+    return ps[ps.length - 1];
+  }
+
+  function _addThumb(opt) {
+    pageIndex++;
+    var locals = {
+      src: opt.src,
+      num: pageIndex,
+      left: curLeft + "px",
+      top: curTop + "px",
+      width: thumbWidth + "px",
+      ref: "page_" + pageIndex
+    }
+    $(thumbTpl(locals)).appendTo("#list .thumbList");
+    curLeft += thumbWidth + gutter;
+  }
+
+  function addChapter(data) {
+    var imgName = _getImageName(data.img.src);
+    if (/^c/.test(imgName)) {
+      console.log('add chapter %s', imgName);
+      _addThumb({
+        src: data.img.src,
+        isNewChapter: true
+      });
+    }
+  }
+
+  function addThumb(data) {
+    var imgName = _getImageName(data.img.src);
+    if (/^t/.test(imgName)) {
+      console.log('add thumb %s', imgName);
+    }
+  }
+
+  function addPage(data) {
+    var imgName = _getImageName(data.img.src);
+    if (/^\d/.test(imgName)) {
+      console.log('add page %s', imgName);
+    }
+  }
+
+  function _end(data) {}
   init();
   console.log('done');
 });
---
+++ src/js/template.js
@@ -0,0 +1,20 @@
+/**
+  {
+    src: "aaa",
+    num: 1,
+    left: "10px",
+    top: "10px",
+    width: "200px",
+    ref: "aaa"
+  }
+ */
+function thumbTpl(locals) {
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    var locals_for_with = locals || {};
+    (function(left, num, ref, src, top, width) {
+        buf.push("<a" + jade.attr("style", "width:" + width + ";left:" + left + ";top:" + top + "", true, false) + jade.attr("data-ref", "" + ref + "", true, false) + ' class="thumb"><span class="pageNum">' + jade.escape((jade_interp = num) == null ? "" : jade_interp) + "</span><img" + jade.attr("src", "" + src + "", true, false) + "/></a>");
+    }).call(this, "left" in locals_for_with ? locals_for_with.left : typeof left !== "undefined" ? left : undefined, "num" in locals_for_with ? locals_for_with.num : typeof num !== "undefined" ? num : undefined, "ref" in locals_for_with ? locals_for_with.ref : typeof ref !== "undefined" ? ref : undefined, "src" in locals_for_with ? locals_for_with.src : typeof src !== "undefined" ? src : undefined, "top" in locals_for_with ? locals_for_with.top : typeof top !== "undefined" ? top : undefined, "width" in locals_for_with ? locals_for_with.width : typeof width !== "undefined" ? width : undefined);
+    return buf.join("");
+}
