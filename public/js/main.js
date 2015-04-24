4c0e33d28a739761b9e19da6c6c3e69a09ea023e
Handle Huang <handle.huang0409@gmail.com>
2015-04-24 16:22:04 +0800

    ab

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,26 @@ $(function() {
     preloadImageList = [],
     totalPages = 0,
     pageIndex = 0,
-    thumbWidth = 200,
-    gutter = 20,
+    thumbWidth = 190,
+    totalThumberWidth = 0,
+    chapterWidth = 200,
+    normalPage = 0,
+    gutter = 15,
+    normalPageGutter = 10,
     curLeft = 0,
     curTop = 100,
+    normalPageHeight = 175,
+    listCurrLeft = 0,
+    winWidth = _W = $(window).width(),
+    winHeight = _H = $(window).height(),
+    isread = false,
     chatperWidth = 50;
+  var isPC = is_pc(),
+    isPad = is_ipad(),
+    isMob = is_mobile();
+  var mousedown = "touchstart";
+  var mouseup = "touchend";
+  var mousemove = "touchmove";
 
   function init() {
     // init books
@@ -28,6 +43,61 @@ $(function() {
       progress: _progress,
       end: _end
     });
+    if ((document.ontouchstart == null && isPC) || (!('ontouchstart' in window) && !isPC)) {
+      mousedown = "mousedown";
+      mouseup = "mouseup";
+      mousemove = "mousemove";
+    }
+    document.addEventListener(mousedown, ondown);
+    document.addEventListener(mousemove, onmove);
+    document.addEventListener(mouseup, onup);
+  }
+  var isdown = false;
+  var smx, nmx, tlen, stime, tlistleft;
+
+  function ondown(e) {
+    //e.preventDefault();
+    isdown = true;
+    smx = nmx = isPC ? e.pageX : e.touches[0].pageX;
+    stime = new Date().getTime();
+  }
+  var ssmin = 0.4;
+  function onmove(e) {
+    if (!isdown || isread) return;
+    e.preventDefault();
+    nmx = isPC ? e.pageX : e.touches[0].pageX;
+    tlen = nmx - smx;
+    if (tlen > 0) {
+      islistmove = true;
+    }
+    tlistleft = Math.max(Math.min(0, tlen*ssmin+listCurrLeft),-(totalThumberWidth));
+    //tlistleft = tlen;
+    //console.log('move %s', tlistleft);
+    $("#list .thumbList").css({
+      left: tlistleft
+    });
+  }
+
+  function onup() {
+    if (!isdown) return;
+    isdown = false;
+    setTimeout(function() {
+      islistmove = false;
+    }, 100);
+    if (isread || nmx == smx) return;
+    var left = Math.max(-(totalThumberWidth), Math.min(0, (tlen / (new Date().getTime() - stime) * 1000) * ssmin + tlistleft));
+    $(".list .imgs,#list .thumbList").animate({
+      left: left
+    }, 500, "swing", function() {
+      listCurrLeft = left;
+      //TODO: move slider
+      /*for (var i = listLeft.length - 1; i >= 0; i--) {
+        if (listCurrLeft <= listLeft[i] * ssmin) {
+          cm.moveslider(i);
+          break;
+        }
+      }*/
+    });
   }
 
   function _progress(data) {
@@ -44,22 +114,42 @@ $(function() {
 
   function _addThumb(opt) {
     pageIndex++;
+    var _top = curTop;
+    if (!opt.isNewChapter) {
+      _top = normalPage * normalPageHeight + curTop;
+      normalPage = normalPage == 0 ? 1 : 0;
+    } else {
+      normalPage = 0;
+    }
+    curLeft = opt.isNewChapter ? curLeft + gutter : curLeft;
     var locals = {
       src: opt.src,
       num: pageIndex,
       left: curLeft + "px",
-      top: curTop + "px",
-      width: thumbWidth + "px",
+      top: _top + "px",
+      width: (opt.isNewChapter ? chapterWidth : thumbWidth) + "px",
       ref: "page_" + pageIndex
     }
-    $(thumbTpl(locals)).appendTo("#list .thumbList");
-    curLeft += thumbWidth + gutter;
+    if (normalPage == 0 && !opt.isNewChapter) {
+      $(thumbTpl2(locals)).appendTo("#list .thumbList");
+    } else {
+      $(thumbTpl(locals)).appendTo("#list .thumbList");
+    }
+    if (normalPage == 0 || opt.isNewChapter) {
+      var _w = opt.isNewChapter ? chapterWidth : thumbWidth;
+      //var _g = opt.isNewChapter ? gutter: normalPageGutter;
+      curLeft += _w + normalPageGutter;
+      
+    }
+    if(opt.isNewChapter){
+      totalThumberWidth = curLeft - chapterWidth - gutter*2;
+    }
   }
 
   function addChapter(data) {
     var imgName = _getImageName(data.img.src);
     if (/^c/.test(imgName)) {
-      console.log('add chapter %s', imgName);
+      //console.log('add chapter %s', imgName);
       _addThumb({
         src: data.img.src,
         isNewChapter: true
@@ -70,7 +160,11 @@ $(function() {
   function addThumb(data) {
     var imgName = _getImageName(data.img.src);
     if (/^t/.test(imgName)) {
-      console.log('add thumb %s', imgName);
+      //console.log('add thumb %s', imgName);
+      _addThumb({
+        src: data.img.src,
+        isNewChapter: false
+      });
     }
   }
 
--- src/js/template.js
+++ src/js/template.js
@@ -18,3 +18,14 @@ function thumbTpl(locals) {
     }).call(this, "left" in locals_for_with ? locals_for_with.left : typeof left !== "undefined" ? left : undefined, "num" in locals_for_with ? locals_for_with.num : typeof num !== "undefined" ? num : undefined, "ref" in locals_for_with ? locals_for_with.ref : typeof ref !== "undefined" ? ref : undefined, "src" in locals_for_with ? locals_for_with.src : typeof src !== "undefined" ? src : undefined, "top" in locals_for_with ? locals_for_with.top : typeof top !== "undefined" ? top : undefined, "width" in locals_for_with ? locals_for_with.width : typeof width !== "undefined" ? width : undefined);
     return buf.join("");
 }
+
+function thumbTpl2(locals) {
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    var locals_for_with = locals || {};
+    (function(left, num, ref, src, top, width) {
+        buf.push("<a" + jade.attr("style", "width:" + width + ";left:" + left + ";top:" + top + "", true, false) + jade.attr("data-ref", "" + ref + "", true, false) + ' class="thumb"><img' + jade.attr("src", "" + src + "", true, false) + '/><span class="pageNum">' + jade.escape((jade_interp = num) == null ? "" : jade_interp) + "</span></a>");
+    }).call(this, "left" in locals_for_with ? locals_for_with.left : typeof left !== "undefined" ? left : undefined, "num" in locals_for_with ? locals_for_with.num : typeof num !== "undefined" ? num : undefined, "ref" in locals_for_with ? locals_for_with.ref : typeof ref !== "undefined" ? ref : undefined, "src" in locals_for_with ? locals_for_with.src : typeof src !== "undefined" ? src : undefined, "top" in locals_for_with ? locals_for_with.top : typeof top !== "undefined" ? top : undefined, "width" in locals_for_with ? locals_for_with.width : typeof width !== "undefined" ? width : undefined);
+    return buf.join("");
+}
